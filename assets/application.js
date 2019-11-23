510806ba5668f3f480e5eb870a5fa543955aaf4f
Krzysztof Rewak <krzysztof.rewak@gmail.com>
2019-11-23 10:49:23 +0100

    filtering

--- assets/application.js
+++ assets/application.js
@@ -6,12 +6,33 @@ new Vue({
 			background: "",
 			articles: [],
 			authors: [],
-			searchPhrase: "",
+			tags: [],
+			authorFilter: "",
+			searchedTitle: "",
+			searchedSection: "",
+			searchedAuthorName: "",
+			showMoreAuthors: false,
 		}
 	},
 	computed: {
 		filteredArticles() {
-			return this.articles
+			let articles = this.articles.filter(article => article.title.toLocaleLowerCase().includes(this.searchedTitle.toLocaleLowerCase()) && article.section.toLocaleLowerCase().includes(this.searchedSection.toLocaleLowerCase()))
+
+			if(this.authorFilter) {
+				articles = articles.filter(article => article.authors.includes(this.authorFilter))
+			}
+
+			return articles
+		},
+		filteredAuthors() {
+			let authors = this.authors.filter(article => article.name.toLocaleLowerCase().includes(this.searchedAuthorName.toLocaleLowerCase()))
+			if(!this.showMoreAuthors) {
+				authors = authors.splice(0, 10)
+			}
+			return authors
+		},
+		showMoreAuthorsMenu() {
+			return !this.searchedAuthorName
 		},
 	},
 	mounted() {
@@ -29,10 +50,24 @@ new Vue({
 		fetch("./data/colors.json").then(response => response.json()).then(colors => {
 			this.background = colors[Math.floor(Math.random() * colors.length)]
 		})
+		fetch("./data/tags.json").then(response => response.json()).then(tags => {
+			for(let tag of tags) {
+				this.tags.push(tag)
+			}
+		})
 	},
 	methods: {
 		getBackgroundLabel(color) {
 			return "background: " + color + " !important;"
+		},
+		filterAuthor(author) {
+			if(this.authorFilter === author) {
+				this.authorFilter = ""
+				this.searchedAuthorName = ""
+			} else {
+				this.authorFilter = author
+				this.searchedAuthorName = author
+			}
 		},
 	},
 })
---
+++ build.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types = 1);
+
+$articles = [];
+$authors = [];
+$colors = [];
+$tags = [];
+
+$number = 0;
+while (true) {
+    $filename = "./resources/$number.json";
+    if (!file_exists($filename)) {
+        break;
+    }
+    $number++;
+}
+
+foreach (range($number - 1, 0) as $number) {
+    $filename = "./resources/$number.json";
+    if (!file_exists($filename)) {
+        break;
+    }
+
+    $issue = json_decode(file_get_contents($filename), true);
+
+    foreach ($issue["articles"] as $article) {
+        foreach ($article["authors"] as $author) {
+            if ($author) {
+                $authors[$author] = isset($authors[$author]) ? $authors[$author] + 1 : 1;
+            }
+        }
+        foreach ($article["tags"] as $tag) {
+            if ($tag) {
+                $tags[$tag] = isset($tags[$tag]) ? $tags[$tag] + 1 : 1;
+            }
+        }
+
+        $colors[$issue["color"]] = true;
+
+        $article["issue"] = $issue["issue"];
+        $article["color"] = $issue["color"];
+        $articles[] = $article;
+    }
+    $number++;
+}
+
+arsort($authors);
+foreach ($authors as $author => &$value) {
+    $value = [
+        "name" => $author,
+        "articles" => $value,
+    ];
+}
+$authors = array_values($authors);
+
+arsort($tags);
+foreach ($tags as $tag => &$value) {
+    $value = [
+        "name" => $tag,
+        "articles" => $value,
+    ];
+}
+$tags = array_values($tags);
+
+$colors = array_keys($colors);
+
+file_put_contents("./data/articles.json", json_encode($articles, JSON_UNESCAPED_UNICODE));
+file_put_contents("./data/authors.json", json_encode($authors, JSON_UNESCAPED_UNICODE));
+file_put_contents("./data/colors.json", json_encode($colors, JSON_UNESCAPED_UNICODE));
+file_put_contents("./data/tags.json", json_encode($tags, JSON_UNESCAPED_UNICODE));
